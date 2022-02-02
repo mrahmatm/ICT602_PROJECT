749353dee9fc1086c08890c3dbcc72d3748c66eb
Rahmat Mustafa <2021858398@student.uitm.edu.my>
2022-02-02 21:20:28 +0800

    mapactivity

--- app/src/main/java/com/example/ict602_grpproject/MainActivity.java
+++ app/src/main/java/com/example/ict602_grpproject/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.ict602_grpproject;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -34,21 +39,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-        
-        /*
-        //letak jap, cara keluarkan popup
-        //declarations ni bole ltk as global var
-        Dialog mDialog;
-        //button ni can be anything yg nk trigger popup
-        Button btnShow;
 
-        btnShow.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View v){
-               mDialog.setContentView(R.layout.map_popup);
-           }
-        });
-        */
     }
 
 }
---
+++ app/src/main/java/com/example/ict602_grpproject/MapsActivity.java
@@ -0,0 +1,67 @@
+package com.example.ict602_grpproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.ict602_grpproject.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+        /*
+        //letak jap, cara keluarkan popup
+        //declarations ni bole ltk as global var
+        Dialog mDialog;
+        //button ni can be anything yg nk trigger popup
+        Button btnShow;
+
+        btnShow.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               mDialog.setContentView(R.layout.map_popup);
+           }
+        });
+        */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
