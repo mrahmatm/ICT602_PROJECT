ad16d84ac0ce4b402f607e1b3a3d650ee2c06f45
kimchi <hakimdjhkm@gmail.com>
2022-02-03 19:20:05 +0800

    added restful api

---
+++ app/src/main/assets/www/db.php
@@ -0,0 +1,3 @@
+<?php
+$conn = new mysqli('localhost','1140362','FreePassword12','1140362');
+?>
---
+++ app/src/main/assets/www/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>no</title>
+</head>
+<body>
+<center><h1>no</h1></center>
+</body>
+</html>
