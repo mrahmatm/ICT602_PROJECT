98011faef0c01e6ef09c2e5b9c52ca3039de68a6
kimchi <hakimdjhkm@gmail.com>
2022-02-05 06:23:23 +0800

    no need www here la lol

--- app/src/main/assets/www/db.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = new mysqli('localhost','1140362','FreePassword12','1140362');
-?>
--- app/src/main/assets/www/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <title>no</title>
-</head>
-<body>
-<center><h1>no</h1></center>
-</body>
-</html>
